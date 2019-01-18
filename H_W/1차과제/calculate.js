34da6e13243a5c1946d0ac6c4ecd302accb070e1
Kim-Seung-Joon <wldhel9228@gmail.com>
2019-01-18 17:26:43 +0900

    "직접 만들어본 계산기"

---
+++ H_W/1차과제/calculate.js
@@ -0,0 +1,18 @@
+
+
+var flag = false;
+var result = document.getElementById('result');
+var cal = document.getElementById('cal');
+
+function Operation(num) {
+  if(flag== false){
+    
+  }
+}
+
+function Clear () {
+  if(flag==true){
+    flag = false;
+    result = "";
+  }
+}
